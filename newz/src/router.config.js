555688931a1d8a054b95142532583fe30f06b298
Chris <88316842@qq.com>
2017-07-05 01:41:22 +0800

    提交

---
+++ newz/src/router.config.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Chris on 2017/7/4.
+ */
+import Home from './components/Home.vue'
+import Follow from './components/Follow.vue'
+import Column from './components/Column.vue'
+
+export default[
+    {
+        path:'/home',
+        component:Home
+    },
+    {
+        path:'/follow',
+        component:Follow
+    },
+    {
+        path:'/column',
+        component:Column
+    }
+]
